2896e1576017960e677948c38c0b3b95ef326c82
HenriqueAmbrosano <henriqueambrosano@hotmail.com>
2022-03-10 16:29:28 -0300

    formatacao do documento

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exFixacao2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exFixacao2.js
@@ -1,21 +1,21 @@
 // 1 - Usando o objeto abaixo, utilize for/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
 let names = {
-    person1: 'João',
-    person2: 'Maria',
-    person3: 'Jorge',
-  };
+  person1: "João",
+  person2: "Maria",
+  person3: "Jorge",
+};
 
-  for (let name in names){
-      console.log('Olá', names[name])
-  }
+for (each in names) {
+  console.log("Olá", names[each]);
+}
 
 // 2 - Usando o objeto abaixo, utilize for/in e imprima um console.log com as chaves e valores desse objeto.
 let car = {
-    model: 'A3 Sedan',
-    manufacturer: 'Audi',
-    year: 2020
-  };
+  model: "A3 Sedan",
+  manufacturer: "Audi",
+  year: 2020,
+};
 
-  for(let i in car){
-      console.log(i,':',car[i])
-  }
+for (i in car) {
+  console.log(i, ":", car[i]);
+}
