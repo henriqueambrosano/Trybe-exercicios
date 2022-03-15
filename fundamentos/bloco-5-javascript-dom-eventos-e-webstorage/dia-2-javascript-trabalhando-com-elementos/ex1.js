dbe4ed866bb175886fa5319e2032de55cad9e9e2
HenriqueAmbrosano <henriqueambrosano@hotmail.com>
2022-03-15 16:17:14 -0300

    formatação documento

--- fundamentos/bloco-5-javascript-dom-eventos-e-webstorage/dia-2-javascript-trabalhando-com-elementos/ex1.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-webstorage/dia-2-javascript-trabalhando-com-elementos/ex1.js
@@ -1,35 +1,34 @@
 // Acesse o elemento elementoOndeVoceEsta .
 
-let el1 = document.getElementById('elementoOndeVoceEsta')
+let el1 = document.getElementById("elementoOndeVoceEsta");
 // Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
-el1.parentNode.style.color = 'red'
+el1.parentNode.style.color = "red";
 // Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
-el1.firstElementChild.textContent = 'texto do primeiro filho do filho'
+el1.firstElementChild.textContent = "texto do primeiro filho do filho";
 // Acesse o primeiroFilho a partir de pai .
-let firstChild = document.getElementById('pai').firstElementChild
+let firstChild = document.getElementById("pai").firstElementChild;
 // Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta .
-console.log(el1.previousElementSibling)
+console.log(el1.previousElementSibling);
 // Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
-console.log(el1.nextSibling)
+console.log(el1.nextSibling);
 // Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
-console.log(el1.nextElementSibling)
+console.log(el1.nextElementSibling);
 // Agora acesse o terceiroFilho a partir de pai .
-console.log(document.getElementById('pai').children[2])
-
+console.log(document.getElementById("pai").children[2]);
 
 // parte 2
 // Crie um irmão para elementoOndeVoceEsta .
-let irmao = document.createElement('section')
-irmao.setAttribute('id', 'novoIrmao')
-document.getElementById('pai').appendChild(irmao)
+let irmao = document.createElement("section");
+irmao.setAttribute("id", "novoIrmao");
+document.getElementById("pai").appendChild(irmao);
 // Crie um filho para elementoOndeVoceEsta .
-let filho = document.createElement('p')
-filho.textContent = 'filho de elementoOndeVoceEsta criado'
-el1.appendChild(filho)
+let filho = document.createElement("p");
+filho.textContent = "filho de elementoOndeVoceEsta criado";
+el1.appendChild(filho);
 // Crie um filho para primeiroFilhoDoFilho .
-let filho2 = document.createElement('p');
-filho2.textContent = 'filho do primeiro filho do filho criado'
-el1.firstElementChild.appendChild(filho2)
+let filho2 = document.createElement("p");
+filho2.textContent = "filho do primeiro filho do filho criado";
+el1.firstElementChild.appendChild(filho2);
 // A partir desse filho criado, acesse terceiroFilho .
-let filhoCriado = el1.firstElementChild.firstElementChild
-console.log(filhoCriado.parentElement.parentElement.nextElementSibling)
+let filhoCriado = el1.firstElementChild.firstElementChild;
+console.log(filhoCriado.parentElement.parentElement.nextElementSibling);
