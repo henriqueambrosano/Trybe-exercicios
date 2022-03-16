a99e91c8d3d42ab48b8ce3ab777b76d2d511723f
HenriqueAmbrosano <henriqueambrosano@hotmail.com>
2022-03-16 19:46:39 -0300

    exercicio 1 finalizado

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-webstorage/dia-3-javascript-eventos/calendario-tryber/script.js
@@ -0,0 +1,42 @@
+function createDaysOfTheWeek() {
+  const weekDays = [
+    "Domingo",
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sábado",
+  ];
+  const weekDaysList = document.querySelector(".week-days");
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement("li");
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  }
+}
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+// exercicio 1:
+const dezDaysList = [
+  29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+];
+let ulDays = document.getElementById("days");
+for (num of dezDaysList) {
+  let li = document.createElement("li");
+  li.classList = "day";
+  if (num === 24 || num === 25 || num === 31) {
+    li.classList.add("holiday");
+  }
+  if (num === 4 || num === 11 || num === 18 || num === 25) {
+    li.classList.add("friday");
+  }
+  li.textContent = num;
+  ulDays.appendChild(li);
+}
