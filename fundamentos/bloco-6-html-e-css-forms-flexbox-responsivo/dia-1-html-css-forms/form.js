074d335e60f139e50577c42e9648e2bd665f59a9
HenriqueAmbrosano <henriqueambrosano@hotmail.com>
2022-03-24 16:05:45 -0300

    exercicios bonus concluidos

--- fundamentos/bloco-6-html-e-css-forms-flexbox-responsivo/dia-1-html-css-forms/form.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-responsivo/dia-1-html-css-forms/form.js
@@ -1,9 +1,23 @@
 const btnEnviar = document.querySelector('#enviar');
 const btnApagar = document.querySelector('#apagar')
+const nome = document.querySelector('#nome');
+const email = document.querySelector('#email');
+const whyMe = document.querySelector('#whyme');
+
+
+
+
+
 
 
 btnEnviar.onclick = (e) => {
+    console.log(whyMe.value.length)
     e.preventDefault();
+    if(nome.value.length > 40 || nome.value.length < 10 || email.value.length <10 || email.value.length > 50 || whyMe.value.length > 500){
+        alert('Dados inválidos!')
+    }else{
+        alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.')
+    }
 }
 
 btnApagar.onclick = () => {
