cb68598078b4e06f139d3624045a761200a51b3a
HenriqueAmbrosano <henriqueambrosano@hotmail.com>
2022-04-07 19:11:21 -0300

    ex bonus concluido

--- fundamentos/bloco-8-highet-order-functions-do-javascript-es6/dia-1-introducao-a-higher-order-functions/bonus.js
+++ fundamentos/bloco-8-highet-order-functions-do-javascript-es6/dia-1-introducao-a-higher-order-functions/bonus.js
@@ -11,9 +11,6 @@ const warrior = {
   weaponDmg: 2,
   damage: undefined,
 };
-// 15 e 50
-// 30 e 60
-// 45 e 90
 
 const dragon = {
   healthPoints: 350,
@@ -23,29 +20,55 @@ const dragon = {
 
 const battleMembers = { mage, warrior, dragon };
 
-const dragonDamage = () => Math.floor(Math.random() * (dragon.strength -15 +1)) + 15;
+const dragonDamage = () =>
+  Math.floor(Math.random() * (dragon.strength - 15 + 1)) + 15;
 
-const warriorDamage = () => Math.floor(Math.random() * (warrior.strength * warrior.weaponDmg - warrior.strength +1)) + warrior.strength;
+const warriorDamage = () =>
+  Math.floor(
+    Math.random() *
+      (warrior.strength * warrior.weaponDmg - warrior.strength + 1)
+  ) + warrior.strength;
 
 const magePower = () => {
-    if(mage.mana < 15){
-        return {
-            damage: 'Não possui mana suficiente',
-            manaGasta: 0
-        }
-    }else{
-        return {
-        damage: Math.floor(Math.random() * (mage.intelligence * 2 - mage.intelligence +1)) + mage.intelligence,
-        manaGasta: 15,
-    }
-    }
-
-}
-
-// console.log('dragon', dragonDamage())
-// console.log('warrior', warriorDamage())
-// console.log('mage', magePower())
+  if (mage.mana < 15) {
+    return {
+      damage: 'Não possui mana suficiente',
+      manaGasta: 0,
+    };
+  } else {
+    return {
+      damage:
+        Math.floor(
+          Math.random() * (mage.intelligence * 2 - mage.intelligence + 1)
+        ) + mage.intelligence,
+      manaGasta: -15,
+    };
+  }
+};
 
 const gameActions = {
+  warriorAction: (callback) => {
+    warrior.damage = callback();
+    dragon.healthPoints -= warrior.damage;
+  },
 
-}
+  mageAction: (callback) => {
+    mage.damage = callback().damage;
+    mage.mana += callback().manaGasta;
+    dragon.healthPoints -= mage.damage;
+  },
+
+  dragonAction: (callback) => {
+    dragon.damage = callback();
+    mage.healthPoints -= dragon.damage;
+    warrior.healthPoints -= dragon.damage;
+  },
+
+  finalResult: () => battleMembers,
+};
+
+gameActions.warriorAction(warriorDamage);
+gameActions.mageAction(magePower);
+gameActions.dragonAction(dragonDamage);
+
+console.log(gameActions.finalResult());
