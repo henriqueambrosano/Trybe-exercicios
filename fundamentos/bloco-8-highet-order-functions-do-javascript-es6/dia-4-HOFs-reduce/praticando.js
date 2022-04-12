c1adc3312c8aee3ae15228ca631ba62b5b1dccb2
HenriqueAmbrosano <henriqueambrosano@hotmail.com>
2022-04-12 16:04:21 -0300

    ex introdutorio concluido

---
+++ fundamentos/bloco-8-highet-order-functions-do-javascript-es6/dia-4-HOFs-reduce/praticando.js
@@ -0,0 +1,91 @@
+// somar apenas os elementos pares de um array com a função reduce
+
+const myArray = [1, 2, 3];
+
+console.log(myArray.reduce((a, b) => (b % 2 === 0 ? a + b : a), 0));
+
+// somar apenas os elementos pares de um array com a função filter e reduce
+
+console.log(myArray.filter((el) => el % 2 === 0).reduce((a, b) => a + b));
+
+const estudantes = [
+  {
+    nome: 'Jorge',
+    sobrenome: 'Silva',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 67 },
+      { name: 'Português', nota: 79 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 65 },
+    ],
+  },
+  {
+    nome: 'Mario',
+    sobrenome: 'Ferreira',
+    idade: 15,
+    turno: 'Tarde',
+    materias: [
+      { name: 'Matemática', nota: 59 },
+      { name: 'Português', nota: 80 },
+      { name: 'Química', nota: 78 },
+      { name: 'Biologia', nota: 92 },
+    ],
+  },
+  {
+    nome: 'Jorge',
+    sobrenome: 'Santos',
+    idade: 15,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 76 },
+      { name: 'Português', nota: 90 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 80 },
+    ],
+  },
+  {
+    nome: 'Maria',
+    sobrenome: 'Silveira',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 91 },
+      { name: 'Português', nota: 85 },
+      { name: 'Química', nota: 92 },
+      { name: 'Biologia', nota: 90 },
+    ],
+  },
+  {
+    nome: 'Natalia',
+    sobrenome: 'Castro',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 70 },
+      { name: 'Português', nota: 70 },
+      { name: 'Química', nota: 60 },
+      { name: 'Biologia', nota: 50 },
+    ],
+  },
+  {
+    nome: 'Wilson',
+    sobrenome: 'Martins',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 80 },
+      { name: 'Português', nota: 82 },
+      { name: 'Química', nota: 79 },
+      { name: 'Biologia', nota: 75 },
+    ],
+  },
+];
+
+const expectedResult = estudantes.map((estudante) => ({
+  nome: estudante.nome,
+  materia: estudante.materias.reduce((a, b) => (a.nota > b.nota ? a : b)).name,
+}));
+
+console.log(expectedResult);
