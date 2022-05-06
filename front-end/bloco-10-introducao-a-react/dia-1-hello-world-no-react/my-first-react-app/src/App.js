90a62e4dec916a64467296ffc158ad4aeca75a1c
HenriqueAmbrosano <henriqueambrosano@hotmail.com>
2022-05-05 21:43:13 -0300

    bonus concluido

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/my-first-react-app/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/my-first-react-app/src/App.js
@@ -2,15 +2,6 @@ import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
 
-
-const compromissos = ['c1','c2','c3','c4','c5','c6']
-
-const Task = (value) => {
-  return (    
-    <li>{value}</li>
-  );
-}
-
 function App() {
   return(
     <div>
