7f5cd41979b3dcb9c0f1909e26502af099af7116
HenriqueAmbrosano <henriqueambrosano@hotmail.com>
2022-05-17 19:44:18 -0300

    ex fixacao 2

---
+++ front-end/bloco-12-ciclo-de-vida-de-componentes/dia-1-ciclo-de-vida-dos-componentes/ex-fixacao2/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import User from './User';
+
+class App extends React.Component{
+
+  state = {
+  }
+
+  render () {
+    return (
+      <User/> 
+    )  
+
+  }
+}
+
+export default App;
