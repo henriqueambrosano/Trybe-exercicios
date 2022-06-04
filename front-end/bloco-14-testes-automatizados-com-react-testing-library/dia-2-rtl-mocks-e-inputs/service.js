3686448e1fbd03c5965ace500ce3aaea2182ce8a
HenriqueAmbrosano <henriqueambrosano@hotmail.com>
2022-06-04 20:00:03 -0300

    comit pro push

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/service.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/service.js
@@ -1,5 +1,36 @@
-function randomNumber() {
-  return Math.floor(Math.random()*101);
-} 
+import fetch from "node-fetch";
 
-module.exports = { randomNumber }
+function randomNumber() {
+  return Math.floor(Math.random() * 101);
+}
+
+function toUpper(string) {
+  return string.toUpperCase();
+}
+
+function firstLetter(string) {
+  return string[0];
+}
+
+function joinStrings(s1, s2) {
+  return s1 + s2;
+}
+
+async function requestDog() {
+  fetch('https://dog.ceo/api/breeds/list/all').then((response) =>
+    response
+      .json()
+      .then(response.ok ? Promise.resolve(json) : Promise.reject(json))
+    );
+}
+
+requestDog()
+
+
+module.exports = {
+  toUpper,
+  firstLetter,
+  joinStrings,
+  randomNumber,
+  requestDog,
+};
